2d9e41ab6fc27e9f1b29c40e34ce05c69e96f62c
Aarti-Chaudhary <nehac9459@gmail.com>
2025-05-20 10:36:07 +0530

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// let n=prompt("enter number:");
+// let arr=[];
+// for(let i=1;i<=n;i++){
+// arr[i-1]=i;
+// }
+// let narr=arr.reduce((performance,curr) =>{
+//     return  performance*curr;
+    
+// })
+// console.log(narr);
